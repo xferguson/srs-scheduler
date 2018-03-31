3762533af6b059655e2b4d8ee7d0035ced5408a0
Alexander Ferguson <x@echonotation.com>
2018-03-31 13:53:07 -0600

    Move batch and total days to props

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,5 +1,6 @@
 /* eslint-env node, browser */
 /* global Plotly, React, ReactDOM, createPlotlyComponent */
+import PropTypes from "prop-types";
 import Review from "./review.js";
 // import React from "react";
 // import Plot from "react-plotly.js"; 
@@ -8,46 +9,58 @@ const Plot = createPlotlyComponent(Plotly),
     studySession = new Review();
 
 let batch = 20,
-    totalDays = 90,
-    studySessionReviews;
+    totalDays = 90;
 
-// class ReviewChart extends React.Component { // eslint-disable-line no-unused-vars
-//     render() {
-//         return (
-//             <Plot
-//                 data={[
-//                   {
-//                     x: [1, 2, 3],
-//                     y: [2, 6, 3],
-//                     type: "scatter",
-//                     mode: "lines+points",
-//                     marker: {color: "red"},
-//                   },
-//                   {type: "bar", x: [1, 2, 3], y: [2, 5, 3]},
-//                 ]}
-//                 layout={ {width: 320, height: 240, title: "A Fancy Plot"} }
-//               />
-//         );
-//     }
-// }
-
-studySessionReviews = studySession.getReviewSchedule(batch, totalDays);
-
-ReactDOM.render(
-    React.createElement(Plot, {
-        data: [{
-            type: "bar",
-            x: [...Array(studySessionReviews.length)
+class ReviewChart extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            batch: props.batch,
+            totalDays: props.totalDays
+        };
+    }
+    getSessionReviews(batch, totalDays) {
+        return studySession.getReviewSchedule(batch, totalDays);
+    }
+    render() {
+        const studySessionReviews = this.getSessionReviews(this.state.batch, this.state.totalDays),
+            plotX = [...Array(studySessionReviews.length)
                 .fill(undefined)
                 .map((item, index) => index + 1)
             ],
-            y: [...studySessionReviews]
-        }],
-        layout: {
-            width: 600,
-            height: 480,
-            title: "Review Schedule"
-        }
-    }),
+            plotY = [...studySessionReviews];
+
+        return ( 
+            <div>
+                <Plot
+                    data = {
+                        [{
+                            type: "bar",
+                            x: plotX,
+                            y: plotY
+                        }]
+                    }
+                    layout = {
+                        {
+                            width: 600,
+                            height: 480,
+                            title: "Review Schedule"
+                        }
+                    }
+                />
+            </div>
+        );
+    }
+}
+ReviewChart.propTypes = {
+    batch: PropTypes.number,
+    totalDays: PropTypes.number
+};
+
+ReactDOM.render(
+    <ReviewChart
+        batch={batch}
+        totalDays={totalDays}
+    /> ,
     document.getElementById("root")
 );
