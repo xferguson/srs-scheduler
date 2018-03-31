5d2ca4d96bad9218d7b7ffcb1c9ff9b749989fe3
Alexander Ferguson <x@echonotation.com>
2018-03-31 14:35:00 -0600

    Add inputs to edit the Daily New Cards and Numbber of Days

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -14,21 +14,53 @@ let batch = 20,
 class ReviewChart extends React.Component {
     constructor(props) {
         super(props);
+
+        /* State */
         this.state = {
             batch: props.batch,
             totalDays: props.totalDays
         };
+
+        /* Bindings */
+        this.getSessionReviews = this.getSessionReviews.bind(this);
+        this.updateBatch = this.updateBatch.bind(this);
+        this.updateTotalDays = this.updateTotalDays.bind(this);
     }
     getSessionReviews(batch, totalDays) {
         return studySession.getReviewSchedule(batch, totalDays);
     }
+
+    parseToNumber(val) {
+        // Current stopgap helper for an issue with numbers being rendered as strings
+        // Found in this thread: https://github.com/erikras/redux-form/issues/2940
+        return isNaN(parseInt(val, 10)) ? null : parseInt(val, 10);
+    }
+    updateBatch(event) {
+        let newBatch = this.parseToNumber(event.target.value);
+        this.setState({
+            batch: newBatch
+        });
+    }
+    updateTotalDays(event) {
+        let newTotalDays = this.parseToNumber(event.target.value);
+        this.setState({
+            totalDays: newTotalDays
+        });
+    }
     render() {
-        const studySessionReviews = this.getSessionReviews(this.state.batch, this.state.totalDays),
+        const that = this;
+        const studySessionReviews = that.getSessionReviews(that.state.batch, that.state.totalDays),
             plotX = [...Array(studySessionReviews.length)
                 .fill(undefined)
                 .map((item, index) => index + 1)
             ],
             plotY = [...studySessionReviews];
+        function getTotalStudied() {
+            const totalNew = that.state.batch * that.state.totalDays;
+            return (
+                    <p>At that rate, you will learn {totalNew} flash cards.</p>
+                );
+        }
 
         return ( 
             <div>
@@ -48,6 +80,19 @@ class ReviewChart extends React.Component {
                         }
                     }
                 />
+                <form>
+                    <label>
+                        Daily New Cards:
+                        <input type="number" min="0" max="200" value={that.state.batch} onChange={that.updateBatch} />
+                    </label>
+                    <br />
+                    <br />
+                    <label>
+                        Number of Days:
+                        <input type="number" min="0" max="730" value={that.state.totalDays} onChange={that.updateTotalDays} />
+                    </label>
+                    {getTotalStudied()}
+                </form>
             </div>
         );
     }
