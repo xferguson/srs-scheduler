067e354d1e5b1a9b138e817d0619653db5a27263
Alexander Ferguson <x@echonotation.com>
2018-04-10 09:45:18 -0600

    Abstract review chart to separate file

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,111 +1,16 @@
 /* eslint-env node, browser */
-/* global Plotly, React, ReactDOM, createPlotlyComponent */
-import PropTypes from "prop-types";
-import Review from "./review.js";
+/* global React, ReactDOM*/
 // import React from "react";
 // import Plot from "react-plotly.js"; 
-
-const Plot = createPlotlyComponent(Plotly),
-    studySession = new Review();
+import ReviewChart from "./review-chart.jsx";
 
 let batch = 20,
     totalDays = 90;
-
-class ReviewChart extends React.Component {
-    constructor(props) {
-        super(props);
-
-        /* State */
-        this.state = {
-            batch: props.batch,
-            totalDays: props.totalDays
-        };
-
-        /* Bindings */
-        this.getSessionReviews = this.getSessionReviews.bind(this);
-        this.updateBatch = this.updateBatch.bind(this);
-        this.updateTotalDays = this.updateTotalDays.bind(this);
-    }
-    getSessionReviews(batch, totalDays) {
-        return studySession.getReviewSchedule(batch, totalDays);
-    }
-
-    parseToNumber(val) {
-        // Current stopgap helper for an issue with numbers being rendered as strings
-        // Found in this thread: https://github.com/erikras/redux-form/issues/2940
-        return isNaN(parseInt(val, 10)) ? null : parseInt(val, 10);
-    }
-    updateBatch(event) {
-        let newBatch = this.parseToNumber(event.target.value);
-        this.setState({
-            batch: newBatch
-        });
-    }
-    updateTotalDays(event) {
-        let newTotalDays = this.parseToNumber(event.target.value);
-        this.setState({
-            totalDays: newTotalDays
-        });
-    }
-    render() {
-        const that = this;
-        const studySessionReviews = that.getSessionReviews(that.state.batch, that.state.totalDays),
-            plotX = [...Array(studySessionReviews.length)
-                .fill(undefined)
-                .map((item, index) => index + 1)
-            ],
-            plotY = [...studySessionReviews];
-        function getTotalStudied() {
-            const totalNew = that.state.batch * that.state.totalDays;
-            return (
-                    <p>At that rate, you will learn {totalNew} flash cards.</p>
-                );
-        }
-
-        return ( 
-            <div>
-                <Plot
-                    data = {
-                        [{
-                            type: "bar",
-                            x: plotX,
-                            y: plotY
-                        }]
-                    }
-                    layout = {
-                        {
-                            width: 600,
-                            height: 480,
-                            title: "Review Schedule"
-                        }
-                    }
-                />
-                <form>
-                    <label>
-                        Daily New Cards:
-                        <input type="number" min="0" max="200" value={that.state.batch} onChange={that.updateBatch} />
-                    </label>
-                    <br />
-                    <br />
-                    <label>
-                        Number of Days:
-                        <input type="number" min="0" max="730" value={that.state.totalDays} onChange={that.updateTotalDays} />
-                    </label>
-                    {getTotalStudied()}
-                </form>
-            </div>
-        );
-    }
-}
-ReviewChart.propTypes = {
-    batch: PropTypes.number,
-    totalDays: PropTypes.number
-};
 
 ReactDOM.render(
     <ReviewChart
         batch={batch}
         totalDays={totalDays}
-    /> ,
+    />,
     document.getElementById("root")
 );
--- src/js/review.js
+++ src/js/review.js
@@ -1,56 +1,75 @@
 /* eslint-env node */
 class Review {
-    constructor() {
-        this.intervals = [1, 1, 6, 12, 24, 48, 96, 180];
-    }
+    setIntervalMode(mode) {
+        if (this.intervalModes.hasOwnProperty(mode)) {
+            this.intervals = this.intervalModes[mode];
+        } else {
+            return false;
+        }
+    } 
 
-    getReviewSchedule(batchSize, days) {
-
-        var intervals = this.intervals;
-        var dailyBatches = [];
-        var reviewsByDay = [0];
-
-        class Batch {
-            constructor(batchSize) {
-                this.batchSize = batchSize;
-                this.intervals = intervals;
-                this.interval = 0;
-                this.daysUntilReview = intervals[0];
+    getReviewSchedule(batchSize, days, mode = "Memrise") {
+        const intervalModes = {
+            Memrise: [1, 1, 6, 12, 24, 48, 96, 180],
+            ClozeMaster: [],
+            Anki: []
+        };
+        const getIntervals = (mode) => {
+            if (intervalModes.hasOwnProperty(mode)) {
+                return intervalModes[mode];
+            } else {
+                return false;
             }
+        };
+        const intervals = getIntervals(mode);
+        if (intervals) {
+            let dailyBatches = [];
+            let reviewsByDay = [0];
 
-            reviewsToday() {
-                if (this.daysUntilReview === 0) {
-                    return this.batchSize;
-                } else {
-                    return 0;
+            class Batch {
+                constructor(batchSize) {
+                    this.batchSize = batchSize;
+                    this.intervals = intervals;
+                    this.interval = 0;
+                    this.daysUntilReview = intervals[0];
                 }
-            }
 
-            advanceInterval() {
-                if (this.daysUntilReview > 0) {
-                    this.daysUntilReview = this.daysUntilReview - 1;
-                } else {
-                    this.interval = this.interval + 1;
-                    if (this.interval < this.intervals.length) {
-                        this.daysUntilReview = this.intervals[this.interval];
+                reviewsToday() {
+                    if (this.daysUntilReview === 0) {
+                        return this.batchSize;
                     } else {
-                        this.daysUntilReview = this.intervals[this.intervals.length - 1];
+                        return 0;
+                    }
+                }
+
+                advanceInterval() {
+                    if (this.daysUntilReview > 0) {
+                        this.daysUntilReview = this.daysUntilReview - 1;
+                    } else {
+                        this.interval = this.interval + 1;
+                        if (this.interval < this.intervals.length) {
+                            this.daysUntilReview = this.intervals[this.interval];
+                        } else {
+                            this.daysUntilReview = this.intervals[this.intervals.length - 1];
+                        }
                     }
                 }
             }
-        }
 
-        for (var i = 0; i < days; i++) {
-            dailyBatches.push(new Batch(batchSize));
-            for (var d = 0; d < dailyBatches.length; d++) {
-                reviewsByDay[i] = reviewsByDay[i] ? reviewsByDay[i] + dailyBatches[d].reviewsToday() : 0 + dailyBatches[d].reviewsToday();
-                dailyBatches[d].advanceInterval();
+            for (let i = 0; i < days; i++) {
+                dailyBatches.push(new Batch(batchSize));
+                for (var d = 0; d < dailyBatches.length; d++) {
+                    reviewsByDay[i] = reviewsByDay[i] ? reviewsByDay[i] + dailyBatches[d].reviewsToday() : 0 + dailyBatches[d].reviewsToday();
+                    dailyBatches[d].advanceInterval();
+                }
             }
-        }
 
-        return reviewsByDay;
+            return reviewsByDay;
+        } else {
+            return false;
+        }
     }
 
 }
 
-module.exports = Review;
+export default Review;
